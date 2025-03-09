d67830c2ff9de191e24ab8fbd3c0896bdb2ee475
AMJADKHANAK1147 <amjadkhanak1221@gmail.com>
2025-03-09 12:09:00 +0530

    09-03-2025

---
+++ aggressiveCow.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+
+int main(){
+
+    int N =5;
+   vector<int>vec = {1,2,8,4,9};
+    int cow  = 3;
+
+    sort(vec.begin(),vec.end());
+
+  
+    
+
+
+
+
+    return 0;
+}
